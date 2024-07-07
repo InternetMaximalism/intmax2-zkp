57d8f6198a0bf572c0c19dcd8e5239dbda6c4d9f
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-07-07 21:32:16 +0800

    add recipient

--- src/circuits/mining/simple_withraw_circuit.rs
+++ src/circuits/mining/simple_withraw_circuit.rs
@@ -24,6 +24,7 @@ use crate::{
     },
     constants::DEPOSIT_TREE_HEIGHT,
     ethereum_types::{
+        address::Address,
         bytes32::Bytes32,
         u256::U256,
         u32limb_trait::{U32LimbTargetTrait as _, U32LimbTrait as _},
@@ -39,6 +40,7 @@ use crate::{
 pub struct SimpleWithdrawPublicInputs {
     pub deposit_root: Bytes32<u32>,
     pub nullifier: Bytes32<u32>, // nullifier = hash(pubkey, zero_salt)
+    pub recipient: Address<u32>,
     pub token_index: u32,
     pub amount: U256<u32>,
 }
@@ -47,6 +49,7 @@ pub struct SimpleWithdrawPublicInputs {
 pub struct SimpleWithdrawPublicInputsTarget {
     pub deposit_root: Bytes32<Target>,
     pub nullifier: Bytes32<Target>,
+    pub recipient: Address<Target>,
     pub token_index: Target,
     pub amount: U256<Target>,
 }
@@ -56,6 +59,7 @@ impl SimpleWithdrawPublicInputs {
         let vec = vec![
             self.deposit_root.limbs(),
             self.nullifier.limbs(),
+            self.recipient.limbs(),
             vec![self.token_index],
             self.amount.limbs(),
         ]
@@ -74,6 +78,7 @@ impl SimpleWithdrawPublicInputsTarget {
         let vec = vec![
             self.deposit_root.limbs(),
             self.nullifier.limbs(),
+            self.recipient.limbs(),
             vec![self.token_index],
             self.amount.limbs(),
         ]
@@ -102,6 +107,7 @@ pub struct SimpleWithdrawValue {
     pub deposit_index: u32,
     pub deposit_leaf: DepositLeaf,
     pub deposit_merkle_proof: DepositMerkleProof,
+    pub recipient: Address<u32>,
     pub nullifier: Bytes32<u32>,
     pub pubkey: U256<u32>,
     pub salt: Salt,
@@ -112,6 +118,7 @@ pub struct SimpleWithdrawTarget {
     pub deposit_index: Target,
     pub deposit_leaf: DepositLeafTarget,
     pub deposit_merkle_proof: DepositMerkleProofTarget,
+    pub recipient: Address<Target>,
     pub nullifier: Bytes32<Target>,
     pub pubkey: U256<Target>,
     pub salt: SaltTarget,
@@ -123,6 +130,7 @@ impl SimpleWithdrawValue {
         deposit_index: u32,
         deposit_leaf: DepositLeaf,
         deposit_merkle_proof: DepositMerkleProof,
+        recipient: Address<u32>,
         pubkey: U256<u32>,
         salt: Salt,
     ) -> Self {
@@ -140,6 +148,7 @@ impl SimpleWithdrawValue {
             deposit_index,
             deposit_leaf,
             deposit_merkle_proof,
+            recipient,
             nullifier,
             pubkey,
             salt,
@@ -169,11 +178,17 @@ impl SimpleWithdrawTarget {
             .connect(builder, pubkey_salt_hash);
         let default_salt = SaltTarget::constant(builder, Salt::default());
         let nullifier = get_pubkey_salt_hash_circuit(builder, pubkey, default_salt);
+        let recipient = Address::new(builder, false);
+        // add constraints to bind address.
+        for limb in recipient.limbs() {
+            let _ = builder.mul(limb, limb);
+        }
         Self {
             deposit_root,
             deposit_index,
             deposit_leaf,
             deposit_merkle_proof,
+            recipient,
             nullifier,
             pubkey,
             salt,
@@ -193,6 +208,7 @@ impl SimpleWithdrawTarget {
         self.deposit_leaf.set_witness(witness, &value.deposit_leaf);
         self.deposit_merkle_proof
             .set_witness(witness, &value.deposit_merkle_proof);
+        self.recipient.set_witness(witness, value.recipient);
         self.nullifier.set_witness(witness, value.nullifier);
         self.pubkey.set_witness(witness, value.pubkey);
         self.salt.set_witness(witness, value.salt);
@@ -234,6 +250,7 @@ where
         let target = SimpleWithdrawTarget::new::<F, C, D>(&mut builder);
         let pis = SimpleWithdrawPublicInputsTarget {
             deposit_root: target.deposit_root,
+            recipient: target.recipient,
             nullifier: target.nullifier,
             token_index: target.deposit_leaf.token_index,
             amount: target.deposit_leaf.amount,
@@ -278,7 +295,7 @@ mod tests {
             trees::deposit_tree::{DepositLeaf, DepositTree},
         },
         constants::DEPOSIT_TREE_HEIGHT,
-        ethereum_types::u256::U256,
+        ethereum_types::{address::Address, u256::U256, u32limb_trait::U32LimbTrait as _},
         utils::save::{save_circuit_data, save_proof},
         wrapper_config::plonky2_config::PoseidonBN128GoldilocksConfig,
     };
@@ -321,11 +338,13 @@ mod tests {
 
         let deposit_merkle_proof = deposit_tree.prove(deposit_index);
 
+        let recipient = Address::rand(&mut rng);
         let value = SimpleWithdrawValue::new(
             deposit_tree.get_root(),
             deposit_index as u32,
             deposit,
             deposit_merkle_proof,
+            recipient,
             pubkey,
             salt,
         );
@@ -370,11 +389,13 @@ mod tests {
 
         let deposit_merkle_proof = deposit_tree.prove(deposit_index);
 
+        let recipient = Address::rand(&mut rng);
         let value = SimpleWithdrawValue::new(
             deposit_tree.get_root(),
             deposit_index as u32,
             deposit,
             deposit_merkle_proof,
+            recipient,
             pubkey,
             salt,
         );
