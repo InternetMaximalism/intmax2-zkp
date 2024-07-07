8fd93cd446b1cf6ac5b8ca250b57baecc91a805b
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-07-07 14:13:34 +0800

    validity circuit wip

--- src/circuits/validity/validity_circuit.rs
+++ src/circuits/validity/validity_circuit.rs
@@ -1,1 +1,72 @@
+use plonky2::{
+    field::extension::Extendable,
+    gates::noop::NoopGate,
+    hash::hash_types::RichField,
+    iop::target::BoolTarget,
+    plonk::{
+        circuit_builder::CircuitBuilder,
+        circuit_data::{CircuitConfig, CircuitData, CommonCircuitData, VerifierCircuitTarget},
+        config::{AlgebraicHasher, GenericConfig},
+        proof::ProofWithPublicInputsTarget,
+    },
+};
 
+use crate::constants::VALIDITY_CIRCUIT_PADDING_DEGREE;
+
+#[derive(Debug)]
+pub struct ValidityCircuit<F, C, const D: usize>
+where
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+{
+    pub data: CircuitData<F, C, D>,
+    pub is_first_step: BoolTarget,
+    pub transition_proof: ProofWithPublicInputsTarget<D>,
+    pub prev_proof: ProofWithPublicInputsTarget<D>,
+    pub verifier_data_target: VerifierCircuitTarget,
+}
+
+impl<F, C, const D: usize> ValidityCircuit<F, C, D>
+where
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+{
+    pub fn new() -> Self {
+        todo!()
+    }
+}
+
+// Generates `CommonCircuitData` usable for recursion.
+pub fn common_data_for_validity_circuit<
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+    const D: usize,
+>() -> CommonCircuitData<F, D>
+where
+    C::Hasher: AlgebraicHasher<F>,
+{
+    let builder = CircuitBuilder::<F, D>::new(CircuitConfig::default());
+    let data = builder.build::<C>();
+
+    let mut builder = CircuitBuilder::<F, D>::new(CircuitConfig::default());
+    let proof = builder.add_virtual_proof_with_pis(&data.common);
+    let verifier_data = VerifierCircuitTarget {
+        constants_sigmas_cap: builder.add_virtual_cap(data.common.config.fri_config.cap_height),
+        circuit_digest: builder.add_virtual_hash(),
+    };
+    builder.verify_proof::<C>(&proof, &verifier_data, &data.common);
+    let data = builder.build::<C>();
+
+    let config = CircuitConfig::standard_recursion_config();
+    let mut builder = CircuitBuilder::<F, D>::new(config);
+    let proof = builder.add_virtual_proof_with_pis(&data.common);
+    let verifier_data = VerifierCircuitTarget {
+        constants_sigmas_cap: builder.add_virtual_cap(data.common.config.fri_config.cap_height),
+        circuit_digest: builder.add_virtual_hash(),
+    };
+    builder.verify_proof::<C>(&proof, &verifier_data, &data.common);
+    while builder.num_gates() < 1 << VALIDITY_CIRCUIT_PADDING_DEGREE {
+        builder.add_gate(NoopGate, vec![]);
+    }
+    builder.build::<C>().common
+}
