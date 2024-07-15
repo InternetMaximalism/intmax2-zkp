aa639265634f1be2c7f79eee9a179eac09c6cbf5
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-07-16 05:02:14 +0800

    del tx info

--- src/common/witness/tx_witness.rs
+++ src/common/witness/tx_witness.rs
@@ -1,9 +1,6 @@
-use crate::common::{
-    trees::{transfer_tree::TransferTree, tx_tree::TxMerkleProof},
-    tx::Tx,
-};
+use crate::common::{trees::tx_tree::TxMerkleProof, tx::Tx};
 
-use super::{transfer_witness::TransferWitness, validity_witness::ValidityWitness};
+use super::validity_witness::ValidityWitness;
 
 /// Information needed to prove that a tx has been included in a block
 #[derive(Debug, Clone)]
@@ -12,30 +9,4 @@ pub struct TxWitness {
     pub tx: Tx,
     pub tx_index: usize,
     pub tx_merkle_proof: TxMerkleProof,
-}
-
-#[derive(Debug, Clone)]
-pub struct TxInfo {
-    pub tx_witness: TxWitness,
-    pub transfer_tree: TransferTree,
-}
-
-impl TxInfo {
-    pub fn generate_transfer_witnesses(&self) -> Vec<TransferWitness> {
-        return self
-            .transfer_tree
-            .leaves()
-            .into_iter()
-            .enumerate()
-            .map(|(transfer_index, transfer)| {
-                let transfer_merkle_proof = self.transfer_tree.prove(transfer_index);
-                TransferWitness {
-                    tx: self.tx_witness.tx.clone(),
-                    transfer: transfer.clone(),
-                    transfer_index,
-                    transfer_merkle_proof,
-                }
-            })
-            .collect();
-    }
 }
