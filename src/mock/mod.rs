763a98dc5533a808feb36638228a91d0bfbeb418
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-11-04 00:20:48 +0700

    chore

--- src/mock/mod.rs
+++ src/mock/mod.rs
@@ -1,11 +1,9 @@
+pub mod balance_logic;
 pub mod block_builder;
+pub mod client;
 pub mod contract;
-// pub mod sync_balance_prover;
 pub mod data;
 pub mod data_store_server;
+pub mod strategy;
 pub mod sync_validity_prover;
 pub mod tx_request;
-// pub mod wallet;
-pub mod balance_logic;
-pub mod client;
-pub mod strategy;
