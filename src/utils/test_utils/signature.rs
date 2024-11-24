5fa5bc60c3483202e99455a46d7fc3f4e5fa9b3c
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-11-24 08:16:16 +0700

    make public signature rand

--- src/utils/test_utils/signature.rs
+++ src/utils/test_utils/signature.rs
@@ -16,7 +16,7 @@ use plonky2_bn254::{curves::g2::G2Target, utils::hash_to_g2::HashToG2 as _};
 use rand::Rng;
 
 impl SignatureContent {
-    pub(crate) fn rand<R: Rng>(rng: &mut R) -> (Vec<KeySet>, Self) {
+    pub fn rand<R: Rng>(rng: &mut R) -> (Vec<KeySet>, Self) {
         let mut key_sets = (0..NUM_SENDERS_IN_BLOCK)
             .map(|_| KeySet::rand(rng))
             .collect::<Vec<_>>();
