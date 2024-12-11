c7781179b3cb348b95663792927b87e34ff6093d
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-12-11 10:24:49 +0700

    safe_split_lo_and_hi

--- src/utils/poseidon_hash_out.rs
+++ src/utils/poseidon_hash_out.rs
@@ -268,7 +268,7 @@ impl Bytes32Target {
             .elements
             .iter()
             .flat_map(|e| {
-                let (low, high) = builder.split_low_high(*e, 32, 32);
+                let (low, high) = safe_split_lo_and_hi(builder, *e);
                 [high, low]
             })
             .collect::<Vec<_>>();
@@ -299,6 +299,24 @@ impl Bytes32Target {
         self.connect(builder, recovered);
         hash_out
     }
+}
+
+// Split the goldilocks field target uniquely into hi and lo parts. x = hi*2^32 + lo
+fn safe_split_lo_and_hi<F: RichField + Extendable<D>, const D: usize>(
+    builder: &mut CircuitBuilder<F, D>,
+    x: Target,
+) -> (Target, Target) {
+    let (lo, hi) = builder.split_low_high(x, 32, 64);
+    // lo and hi are constrained to be 32 bits and x = hi*2^32 + lo mod p
+    // However, when x < 2^32, there are two possible decompositions:
+    // 1) hi = 0, lo = x
+    // 2) hi = 2^32 - 1, lo = x + 1
+    // Therefore, an additional constraint is needed: when hi = 2^32 - 1, lo must be 0
+    let hi_max = builder.constant(F::from_canonical_u64((1 << 32) - 1));
+    let is_hi_max = builder.is_equal(hi, hi_max);
+    let t = builder.mul(is_hi_max.target, lo);
+    builder.assert_zero(t);
+    (lo, hi)
 }
 
 impl Serialize for PoseidonHashOut {
